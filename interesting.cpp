cd7ef52f67e6abe7af7720c7bf339a82ff0c9020
Arian Joseph <arian_joseph@outlook.es>
2024-09-23 21:27:21 -0500

    first commit

---
+++ interesting.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int minExtraChar(string s, vector<string>& dictionary){
+        unordered_set<string> dict(dictionary.begin(), dictionary.end());
+        int n=s.size();
+        vector<int> dp(n+1,n);
+        dp[0]=0;
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<i; j++){
+                string sub=s.substr(j, i-j);
+                if(dict.find(sub)!=dict.end()){
+                    dp[i]=min(dp[i], dp[j]);
+                }
+            }
+            dp[i]=min(dp[i], dp[i-1]+1);
+        }
+        return dp[n];
+    }
+};
+
+int main(){
+
+    return 0;
+}
